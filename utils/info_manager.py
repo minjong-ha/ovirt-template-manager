01d266783a8fdcabed1a4cf08bb65ae14b578d2a
minjong-ha <hmjong1128@gmail.com>
2022-07-07 09:17:50 +0900

    DONE: add info_manager.py

---
+++ utils/info_manager.py
@@ -0,0 +1,17 @@
+"""
+Created by "Minjong Ha" on 2022/07/06
+"""
+
+class InfoManager:
+    """
+    InfoManager has responsibility to list the images in the disks.
+    It can reformant the xml info of the images into the human-readable format.
+    """
+
+    _conf_manager = None
+    
+    def __init__(self, config_manager):
+        self._conf_manager = config_manager
+    
+    # TODO(220706, Minjong Ha: implement list images or templates?)
+
