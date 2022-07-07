5762e663fe36bfe3ea7d3b765681198b6b9fdf4f
minjong-ha <hmjong1128@gmail.com>
2022-07-07 09:18:04 +0900

    DONE: refactoring

--- utils/config_manager.py
+++ utils/config_manager.py
@@ -69,34 +69,51 @@ class ConfigManager:
         self._img_id = config["IMAGE_TRANSFER"]["DISK_ID"]
         self._img_download_path = config["IMAGE_TRANSFER"]["DOWNLOAD_PATH"]
 
+    # default configurations getter
     def get_common_url(self):
-        """return common_url"""
+        """return common_url (FQDN for ovirt-engine)"""
         return self._common_url
 
     def get_common_id(self):
-        """return common_id"""
+        """return common_id (ID for ovirt-engine)"""
         return self._common_id
 
     def get_common_pw(self):
-        """return common_pw"""
+        """return common_pw (PW for ID)"""
         return self._common_pw
 
     def get_cert_api(self):
-        """return cert_api"""
+        """return cert_api (Certification API (URL path)"""
         return self._cert_api
 
     def get_cert_path(self):
-        """return cert_path"""
+        """return cert_path (Path where the certification is)"""
         return self._cert_path
 
     def get_img_api(self):
-        """return img_api"""
+        """return img_api (Image Transfer API (URL path)"""
         return self._img_api
 
     def get_img_download_path(self):
-        """return img_download_path"""
+        """return img_download_path (Path where the image be downloaded)"""
         return self._img_download_path
 
     def get_img_id(self):
-        """return img_id"""
+        """return img_id (Image Transfer ID that issued by ovirt-engine)"""
         return self._img_id
+
+    def get_cert_req_url(self):
+        """return cert_req_url for download certification"""
+        return self._common_url + self._cert_api
+
+    # download manager configurations getter
+    def get_download_req_url(self):
+        """return download_req_url for issue the ticket"""
+        return self._common_url + self._img_api
+
+    def get_closing_url(self, id):
+        """
+        return closing_url for download finalize
+        id: image trasfer id that issued by ovirt-engine
+        """
+        return self._common_url + self._img_api + "/" + id + "/finalize"
