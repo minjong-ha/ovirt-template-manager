b8e914a329992243e7a8ef2b6d716a0dfced3ced
Minjong Ha <minjong_ha@tmax.co.kr>
2022-07-13 15:52:59 +0900

    DONE: add info_list feature. list all templates and each disk_attachments; TODO: give two options. one is config based download, another is list_info based download.

--- utils/info_manager.py
+++ utils/info_manager.py
@@ -2,6 +2,12 @@
 Created by "Minjong Ha" on 2022/07/06
 """
 
+import requests
+import xml.etree.ElementTree as ET
+
+from .config_manager import cert_params
+from .config_manager import common_headers
+
 class InfoManager:
     """
     InfoManager has responsibility to list the images in the disks.
@@ -12,6 +18,38 @@ class InfoManager:
     
     def __init__(self, config_manager):
         self._conf_manager = config_manager
-    
-    # TODO(220706, Minjong Ha: implement list images or templates?)
 
+    def __get_diskattachment(self, id):
+        print(id)
+        url = self._conf_manager.get_template_url() + "/" + id + "/diskattachments"
+        cert_path = self._conf_manager.get_cert_path()
+        common_id = self._conf_manager.get_common_id()
+        common_pw = self._conf_manager.get_common_pw()
+
+        response = requests.get(url, headers=common_headers, verify=cert_path, auth=(common_id, common_pw))
+        root = ET.fromstring(response.text)
+
+        for disk_attachment in root.iter("disk_attachment"):
+            print(disk_attachment.attrib.get("id"))
+		
+
+    def list_all_templates(self):
+        print("LIST ALL TEMPLATES")
+        url = self._conf_manager.get_template_url()
+        cert_path = self._conf_manager.get_cert_path()
+        common_id = self._conf_manager.get_common_id()
+        common_pw = self._conf_manager.get_common_pw()
+
+        response = requests.get(url, headers=common_headers, verify=cert_path, auth=(common_id, common_pw))
+
+        # Now, I have to reformat the xmls to human-readable form
+        # with diskattachments id 
+        root = ET.fromstring(response.text)
+        for template in root.iter("template"):
+            print(template.find("name").text)
+            print(template.find("description").text)
+            print(template.find("comment").text)
+            print(template.find("version").find("version_name").text)
+            print(template.find("version").find("version_number").text)
+            self.__get_diskattachment(template.attrib.get("id"))
+            print("=============================================================")
